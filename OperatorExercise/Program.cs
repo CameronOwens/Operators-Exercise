28baa2ca9611b71b7d1a81c3c37039d7ebd77b5b
CameronOwens <camowens58@gmail.com>
2022-12-08 13:07:22 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var a = 17;
+            //var b = 4;
+
+            //var div = a / b;
+            //var mod = a % b;
+
+            //Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            //var r = 20;
+            //var pi = Math.PI;
+
+            //var areaOfCircle = pi * (r * r);
+
+
+            //Console.WriteLine($"The area of a circle with radius of {r} is {areaOfCircle}");
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            Console.WriteLine($"What is the radius of your circle");
+
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = CalculateArea(radius);
+
+            
+
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
         }
     }
 }
